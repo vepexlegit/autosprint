0b88a87ae9edebb50f95d7931d731520f5ed9e07
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-06 14:47:33 +0400

    Create AutoSprint.java

---
+++ src/main/java/de/vepexlegit/autosprintdebug/AutoSprint.java
@@ -0,0 +1,15 @@
+package de.vepexlegit.autosprintdebug;
+
+public enum AutoSprint {
+    INSTANCE;
+
+    private boolean enabled = true;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
